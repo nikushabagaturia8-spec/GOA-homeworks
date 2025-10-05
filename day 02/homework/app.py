7a7388d3bbd9a64bee35961af25383934b668242
nikushabagaturia8-spec <nikushabagaturia8@gmail.com>
2025-10-05 00:01:08 -0700

    second commit

---
+++ day 02/homework/app.py
@@ -0,0 +1,9 @@
+print("ცვლადში ჩვენ შეგვიძლია შევინახოთ ნებისმიერი სიტყვა და გამოვიტანოთ ტერმინალში")
+
+ojaxi="dimitri,milena,elene,mate"
+print(ojaxi)
+print(67+67)
+mimateba=(59+36)
+print(mimateba)
+magaliti=(42+93)
+print(magaliti)
