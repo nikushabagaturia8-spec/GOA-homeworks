711118a3498301bd13d58292496336a5533292ce
nikushabagaturia8-spec <nikushabagaturia8@gmail.com>
2025-10-12 12:07:39 -0700

    second commit

---
+++ day 04/classwork/app.py
@@ -0,0 +1,2 @@
+name_and_age="nikoloz baghaturia.11 wlis"
+print(name_and_age)
---
+++ day 04/homework/app.py
@@ -0,0 +1,24 @@
+#print() ით ჩვენ შეგვიძლია გამოვიტანოტ ის რაც არის  შენახული ცვლადში
+#კონკატინაცია არის ტექსტების შეერთება ერთმანეთთან
+#
+
+me_var="nikoloz baghaturia" 
+print(me_var)
+
+print("jirafi joze")
+print("sponge bob")
+print("patric")
+print("mr crabs")
+print("phyton")
+
+float=1,89
+float=48,91
+float=9,49
+float=6,7
+float=7,890
+
+integer=100
+integer=400
+integer=700
+integer=67
+integer=1
