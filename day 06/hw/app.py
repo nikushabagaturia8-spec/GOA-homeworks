1aef76b5fced9cc48fe1c42383ab2d5e03832c84
nikushabagaturia8-spec <nikushabagaturia8@gmail.com>
2025-10-23 06:09:11 -0700

    second commit

---
+++ day 06/hw/app.py
@@ -0,0 +1,17 @@
+
+
+
+name="მე მქვია ნიკოლოზ ბაღათურია"
+age="და ვარ 11 წლის"
+print(name+" "+age)
+
+num1=13545,990
+num2=6644454
+print(13545,990 < 6644454)
+
+i_love_programing="becaus i love computers and i want to make some computer games and some apps"
+print(i_love_programing)
+
+names="elene,milena,dimitri,mate,"
+age="elene-13 wlis,milena-35 wlis,dimitri-37 wlis,mate-7 wlis"
+
