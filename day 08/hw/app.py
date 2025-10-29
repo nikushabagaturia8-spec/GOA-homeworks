e8118faa2ab13e191c2bd2f31c198eb341d2c1ee
nikushabagaturia8-spec <nikushabagaturia8@gmail.com>
2025-10-29 11:54:53 -0700

    second commit

---
+++ day 08/hw/app.py
@@ -0,0 +1,18 @@
+print(28794==28794)
+print(85608<8935245688)
+print(63368256 < 456562)
+print(True or False)
+print(True and False)
+
+#or-ის შემთხვევაში ჩვენ გამოგვიტანს ყველგან True-ს მხოლოდ იმ წინადადების გარდა რომელშიც წერია ორივე False
+# ხოლო and აკეთებს პირიქით
+
+x=5
+y=10
+print(x<y)
+#მან გამოგვიტანა პასუხი იყო თუ არა ეს მაგალითი მართალი
+
+Lari=2,7
+Dollar=1
+print(Lari or Dollar)
+print=(Lari and Dollar)
