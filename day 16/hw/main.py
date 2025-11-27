3d0b8333a6ae23de9a59d3c3e1af1714a71c0c76
nikushabagaturia8-spec <nikushabagaturia8@gmail.com>
2025-11-27 02:53:39 -0800

    second commit

---
+++ day 16/hw/main.py
@@ -0,0 +1,27 @@
+print(82>48 or 82<75)
+
+print(67>98 and 756>98)
+print(87<90 or 75<9)
+
+luwi_magaria=True
+
+
+if luwi_magaria:
+    print("i love it")
+
+
+else:
+    ("i dont love it")
+
+
+
+
+
+are_you_18_years_old=False
+
+
+if are_you_18_years_old:
+    print("tqven swrulwlovani brdzandebit")
+
+else:
+    print("jer gaizarde da mere kidev cade")
